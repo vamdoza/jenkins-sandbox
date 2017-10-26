cb06387e47137f3c23e06c83f406c1075c4b1a1b
JJ <vazquezmjj@gmail.com>
2017-10-25 21:07:34 -0700

    added platform structure, and bash scripts dummys

---
+++ jenkins-files/platform-1/test-script-1.bat
@@ -0,0 +1,5 @@
+@echo off
+Title This is your first batch script!
+echo Welcome to batch scripting!
+echo Writing the current date to a text file1.....
+echo %DATE% > date1.txt
---
+++ jenkins-files/platform-2/test-script-2.bat
@@ -0,0 +1,5 @@
+@echo off
+Title This is your first batch script!
+echo Welcome to batch scripting!
+echo Writing the current date to a text file2.....
+echo %DATE% > date2.txt
